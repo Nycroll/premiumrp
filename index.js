0faf417448c3517e2e5e678fb6276522f9b9d0e4
Nycroll <60111348+Nycroll@users.noreply.github.com>
2020-11-08 22:07:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const botConfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client
+
 bot.commands = new Discord.Collection();
 var prefix = botConfig.prefix;
 
